ac63bc8ecf1650d76de6e3898b959e635ce5a5dc
Shanu Biswas <76697973+Shanu911@users.noreply.github.com>
2023-01-12 13:50:49 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.0.2'
+VERSION = '0.0.1'
 DESCRIPTION = 'segy2seg2'
-LONG_DESCRIPTION = 'A package to convert segy seismic file to seg2 (dat) file'
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
 
 # Setting up
 setup(
@@ -14,14 +15,16 @@ setup(
     long_description_content_type="text/markdown",
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
+    
+    py_modules=["segy2seg2", "formats", "header"],             # Name of the python package
+    package_dir={'':'segy2seg2/src'},     # Directory of the source code of the package
     install_requires=["obspy"],
     keywords=['segy', 'seismic', 'seg2', 'segy to seg2 converter', 'segy to dat file converter', 'segy to dat file converter using python' ],
     classifiers=[
-        "Development Status :: 2 - Working",
-        "Intended Audience :: Geophysicst",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
         "Programming Language :: Python :: 3",
-        "Operating System :: Unix",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
+        "Operating System :: OS Independent",
     ]
 )
